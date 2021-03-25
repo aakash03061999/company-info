020c10a3c6344fca72429aa4f9f79242c63a41d6
aakash03061999 <aakashajithkd@gmail.com>
2021-03-25 15:25:31 +0530

    add B

--- src/org/info/EmployeeInfo.java
+++ src/org/info/EmployeeInfo.java
@@ -1,5 +1,12 @@
 package org.info;
 
 public class EmployeeInfo {
+	private void mailId() {
+		System.out.println("greens@123");
+	}
+private void adhaarNum() {
+	System.out.println("34567890123");
 
 }
+}
+	
