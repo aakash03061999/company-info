0ef0ac489a553a22ac2d4c4813bf9f00037f2827
aakash03061999 <aakashajithkd@gmail.com>
2021-03-25 15:07:17 +0530

    add A

--- src/org/info/EmployeeInfo.java
+++ src/org/info/EmployeeInfo.java
@@ -1,5 +1,11 @@
 package org.info;
 
 public class EmployeeInfo {
+	private void empId() {
+		System.out.println("employee id=10");
+	}
+private void empName() {
+	System.out.println("employee name=Mukesh");
 
+}
 }
